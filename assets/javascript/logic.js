136bf9892e25d8d9e8d410448f3de29e4837e64b
Brandon York <30988963+byork2005@users.noreply.github.com>
2017-11-08 12:43:43 -0600

    update 11/8

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,11 @@
 $(document).ready(function()
 {
+    var tempValue;
+    var tempArray;
     var dieArray = [0,0,0,0,0,0];
     var assignArray = [4,6,8,10,12,20];
+    var showArray = [];
+    var totalsArray = [];
     var d4RandomArr = [];
     var d6RandomArr = [];
     var d8RandomArr = [];
@@ -12,12 +16,11 @@ $(document).ready(function()
 
     $(".dieImage").click(function()
     {
-        var tempValue = $(this).attr('data-value');
-        var tempArray = $(this).attr('data-array');
+        tempValue = $(this).attr('data-value');
+        tempArray = $(this).attr('data-array');
         dieArray[tempArray] = $("#d" + tempValue).val();
         dieArray[tempArray]++;
         $("#d" + tempValue).val(dieArray[tempArray]);
-        console.log(dieArray);
     });
 
     $(".submitBtn").click(function()
@@ -30,8 +33,58 @@ $(document).ready(function()
                 dieRandoms[i].push(RandomNum(1, assignArray[i]));
             }
         }
+        displayNumbers();
+        clearArrays();
     });
 
+    $("#standAlone20").click(function()
+    {
+        var d20 = RandomNum(1, 20);
+        $("#textBox").append("D20: {" + d20 + "}" + "\r\n---------------------\r\n");
+        $("#20result").text(d20);
 
+    })
+
+    $("#textClearBtn").click(function()
+    {
+        $("#textBox").text("");
+    })
+
+    function displayNumbers()
+    {
+        var whichDieArray = [];
+        for (var i = 0; i < 6; i++)
+        {
+            if (dieRandoms[i] != "")
+            {
+                showArray.push(dieRandoms[i]);
+                whichDieArray.push(assignArray[i]);
+            }
+        }
+        for (var j = 0; j < showArray.length; j++)
+        {
+            totalsArray[j] = showArray[j].reduce(getSumArray);
+            var sumTotal = totalsArray.reduce(getSumArray);
+            $("#textBox").append("D" + whichDieArray[j] + ": {" + showArray[j] + "}  Sum: " + totalsArray[j] + "\r\n");
+        }
+        $("#textBox").append("Sum Total: " + sumTotal + "\r\n");
+        $("#textBox").append("---------------------\r\n");
+    };
+
+    function clearArrays()
+    {
+        dieArray = [0,0,0,0,0,0];
+        showArray = [];
+        totalsArray = [];
+        whichDieArray = [];
+        d4RandomArr = [];
+        d6RandomArr = [];
+        d8RandomArr = [];
+        d10RandomArr = [];
+        d12RandomArr = [];
+        d20RandomArr = [];
+        dieRandoms = [d4RandomArr, d6RandomArr, d8RandomArr, d10RandomArr, d12RandomArr, d20RandomArr];
+        $(".numberBox").val(0);
+    }
 
 });
--- assets/javascript/myFunctions.js
+++ assets/javascript/myFunctions.js
@@ -4,3 +4,10 @@ function RandomNum(min, max)
 {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
+
+// Add numbers in Array, using reduce() method.
+// var addedNums = array.reduce(getSumArray)
+function getSumArray(total, num)
+{
+    return total + num;
+}
